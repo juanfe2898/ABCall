4b716e35a14fefae08f45c5b30e4f989e3efbd78
Santiago Casallas Bohorquez <scasallasb@MacBook-Air-de-Santiago.local>
2024-09-22 18:01:48 -0500

    ABCall Add get call service

--- rest-api/src/Adapter/in_controller.js
+++ rest-api/src/Adapter/in_controller.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const router = express.Router();
-const countCallByUser = require('../Domain/get-call-by-user');
+const {countCallByUser , getCallService}= require('../Domain/get-call-by-user');
 
-// Use CORS middleware
+
 router.use(cors());
 
-// Ruta para obtener el número de llamadas por usuario
+router.get('/call/user/:id', async (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+
+  if (isNaN(userId)) {
+    return res.status(400).json({ error: 'ID de usuario inválido' });
+  }
+
+  try {
+    const call= await getCallService(userId);
+    res.status(200).json({ userId, call });
+  } catch (error) {
+    console.error('Error al obtener el conteo de llamadas:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+
 router.get('/call/countByUser/user/:id', async (req, res) => {
   const userId = parseInt(req.params.id, 10);
 
@@ -15,8 +31,8 @@ router.get('/call/countByUser/user/:id', async (req, res) => {
   }
 
   try {
-    const callCount = await countCallByUser(userId);
-    res.status(200).json({ userId, callCount });
+    const call= await countCallByUser(userId);
+    res.status(200).json({ userId, call });
   } catch (error) {
     console.error('Error al obtener el conteo de llamadas:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
--- rest-api/src/Domain/get-call-by-user.js
+++ rest-api/src/Domain/get-call-by-user.js
@@ -2,11 +2,21 @@ const callRepository = require('../Adapter/out_mysql');
 
 async function countCallByUser(userId) {
   try {
-    const callCount = await callRepository.getCountByUserId(userId);
-    return callCount;
+    const call = await callRepository.getCountByUserId(userId);
+    return call;
   } catch (error) {
     throw new Error('Error al contar las llamadas para el usuario');
   }
 }
 
-module.exports = countCallByUser;
+
+async function getCallService(userId) {
+  try {
+    const call = await callRepository.getCallByIdRepository(userId);
+    return call;
+  } catch (error) {
+    throw new Error('Error al contar las llamadas para el usuario');
+  }
+}
+
+module.exports = {countCallByUser, getCallService};
