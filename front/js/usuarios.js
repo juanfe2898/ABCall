523c647adf3a1480e26bbdc0249a3a9a94e7a4a5
Santiago Casallas Bohorquez <scasallasb@MacBook-Air-de-Santiago.local>
2024-09-22 19:07:39 -0500

    :sparkles: Fix index usuarios and modified rest

--- front/js/usuarios.js
+++ front/js/usuarios.js
@@ -8,23 +8,23 @@ async function consultarUsuario() {
 
     try {
         // Hacer el fetch con una consulta GET
-       const response = await fetch(`http://10.0.195.238:3000/rest/incident_management/call/countByUser/user/${userId}`);
+       const response = await fetch(`http://10.0.195.238:3000/rest/incident_management/call/user/${userId}`);
        //const response = await fetch(`http://172.212.120.235:3000/rest/incident_management/call/countByUser/user/${userId}`);
         // Verificar si la respuesta fue exitosa
         if (!response.ok) {
             throw new Error('Usuario no encontrado');
         }
 
-        // Parsear la respuesta a JSON
-        const usuario = await response.json();
 
-        // Mostrar los datos del usuario
+        const callResponse = await response.json();
+
         resultado.innerHTML = `
-            <p><strong>id de usuario:</strong> ${usuario.userId}</p>
-            <p><strong>Cantidad de llamada:</strong> ${usuario.callCount}</p>
+            <p><strong>id de usuario:</strong> ${callResponse.idLlamada}</p>
+            <p><strong>Fecha llamada:</strong> ${callResponse.fechaLlamada}</p>
+             <p><strong>Calificacion:</strong> ${callResponse.calificacion}</p>
+            <p><strong>id de usuari:</strong> ${callResponse.idUsuario}</p>
         `;
     } catch (error) {
-        // Mostrar error en caso de que la consulta falle
         resultado.innerHTML = `<p>${error.message}</p>`;
     }
 }
